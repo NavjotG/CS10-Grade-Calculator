b249f0a77b08ffbc323dd2f5596ade6192de9b27
NavjotG <n.ghotra1@share.epsb.ca>
2022-09-27 11:21:52 -0600

    done

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 // JavaScript Code
+
+// IPO Algorithm
+
+// Input
+let CSS1 = +prompt("Enter Client-Side Scripting 1 grade:");
+let SP1 = +prompt("Enter Structured Programming 1 grade:");
+let SP2 = +prompt("Enter Structured Programming 2 grade:");
+let CSS2 = +prompt("Enter Client-Side Scripting 2 grade:");
+let P = +prompt("Enter Project A grade:");
+
+
+console.log(CSS1, SP1, SP2, CSS2, P);
+
+// Process
+let FGRADE = (CSS1 + SP1 + SP2 + CSS2 + P)/5;
+
+// Output
+alert(`Your final grade is: ${FGRADE}`);
