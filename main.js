9059a0544c530d26f95af7b2041780c9b90d85ee
NavjotG <n.ghotra1@share.epsb.ca>
2022-10-11 11:22:24 -0600

    DONE

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 // JavaScript Code
 
 // IPO Algorithm
+document.getElementById("button").addEventListener("click", sayGrade);
 
+function sayGrade () {
 // Input
-let CSS1 = +prompt("Enter Client-Side Scripting 1 grade:");
-let SP1 = +prompt("Enter Structured Programming 1 grade:");
-let SP2 = +prompt("Enter Structured Programming 2 grade:");
-let CSS2 = +prompt("Enter Client-Side Scripting 2 grade:");
-let P = +prompt("Enter Project A grade:");
+let CSS1 = +document.getElementById("cs1").value;
+let SP1 = +document.getElementById("sp1").value;
+let SP2 = +document.getElementById("sp2").value;
+let CSS2 = +document.getElementById("cs2").value;
+let P = +document.getElementById("pA").value;
 
 
 console.log(CSS1, SP1, SP2, CSS2, P);
@@ -16,4 +18,6 @@ console.log(CSS1, SP1, SP2, CSS2, P);
 let FGRADE = (CSS1 + SP1 + SP2 + CSS2 + P)/5;
 
 // Output
-alert(`Your final grade is: ${FGRADE}`);
+document.getElementById("output").innerHTML = FGRADE;
+
+}
