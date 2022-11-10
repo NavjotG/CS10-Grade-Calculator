e864440cb1f2cfb0be682344531cd6e6499368dc
NavjotG <n.ghotra1@share.epsb.ca>
2022-11-10 10:13:20 -0700

    cfyul

--- main.js
+++ main.js
@@ -3,21 +3,19 @@
 // IPO Algorithm
 document.getElementById("button").addEventListener("click", sayGrade);
 
-function sayGrade () {
-// Input
-let CSS1 = +document.getElementById("cs1").value;
-let SP1 = +document.getElementById("sp1").value;
-let SP2 = +document.getElementById("sp2").value;
-let CSS2 = +document.getElementById("cs2").value;
-let P = +document.getElementById("pA").value;
+function sayGrade() {
+  // Input
+  let CSS1 = +document.getElementById("cs1").value;
+  let SP1 = +document.getElementById("sp1").value;
+  let SP2 = +document.getElementById("sp2").value;
+  let CSS2 = +document.getElementById("cs2").value;
+  let P = +document.getElementById("pA").value;
 
+  console.log(CSS1, SP1, SP2, CSS2, P);
 
-console.log(CSS1, SP1, SP2, CSS2, P);
+  // Process
+  let FGRADE = (CSS1 + SP1 + SP2 + CSS2 + P) / 5;
 
-// Process
-let FGRADE = (CSS1 + SP1 + SP2 + CSS2 + P)/5;
-
-// Output
-document.getElementById("output").innerHTML = FGRADE;
-
+  // Output
+  document.getElementById("output").innerHTML = FGRADE;
 }
